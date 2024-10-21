800adb4ffa06a5378c67baaa0179e323208942e4
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-10-21 11:31:49 +0300

    use zod

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,26 +1,45 @@
 'use server';
+
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+const schema = z.object({
+  title: z.string({ invalid_type_error: 'Invalid Title' }),
+  description: z.string({ invalid_type_error: 'Invalid string' }),
+  category: z.string({ invalid_type_error: 'Invalid String' }),
+  price: z.number({ invalid_type_error: 'Invalid Number' }),
+});
 
 export const create = (formData: FormData) => {
-  // Convert FormData to JSON object
-  const formDataObj: any = {};
-  formData.forEach((value, key) => {
-    formDataObj[key] = value;
+  const validatedFields = schema.safeParse({
+    title: formData.get('title'),
+    description: formData.get('description'),
+    category: formData.get('category'),
+    price: formData.get('price'),
   });
 
-  fetch('https://fakestoreapi.com/products', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json', // Specify JSON content
-    },
-    body: JSON.stringify(formDataObj), // Convert formDataObj to JSON string
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log('DATAAAA', data);
-    })
-    .catch((err) => {
-      console.error('Error:', err); // Handle any errors
+  if (!validatedFields.success) {
+    console.log('ERROR');
+    // errors: validatedFields.error.flatten().fieldErrors,
+  } else {
+    const formDataObj: any = {};
+    formData.forEach((value, key) => {
+      formDataObj[key] = value;
     });
-  revalidatePath('/', 'layout');
+
+    fetch('https://fakestoreapi.com/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // Specify JSON content
+      },
+      body: JSON.stringify(formDataObj), // Convert formDataObj to JSON string
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('DATAAAA', data);
+      })
+      .catch((err) => {
+        console.error('Error:', err); // Handle any errors
+      });
+    revalidatePath('/', 'layout');
+  }
 };
